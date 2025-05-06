2489191eb955c32f48e74780ca808273f005313c
Adam Averay <adam@averay.com>
2025-05-06 23:31:11 +0100

    Add JS value filter

--- src/Extensions/ValuesExtension.php
+++ src/Extensions/ValuesExtension.php
@@ -3,12 +3,20 @@ declare(strict_types=1);
 
 namespace Averay\TwigExtensions\Extensions;
 
+use Averay\HtmlBuilder\Html;
 use Averay\TwigExtensions\Nodes\Tests\InstanceOfTest;
 use Twig\Extension\AbstractExtension;
+use Twig\TwigFilter;
 use Twig\TwigTest;
 
 final class ValuesExtension extends AbstractExtension
 {
+  #[\Override]
+  public function getFilters(): array
+  {
+    return [new TwigFilter('js_value', Html\escapeJsValue(...), ['is_safe' => ['html']])];
+  }
+
   #[\Override]
   public function getTests(): array
   {
--- tests/Extensions/Values/ValuesTest.php
+++ tests/Extensions/Values/ValuesTest.php
@@ -13,6 +13,51 @@ use PHPUnit\Framework\Attributes\DataProvider;
 #[CoversClass(InstanceOfTest::class)]
 final class ValuesTest extends TestCase
 {
+  #[DataProvider('jsValueDataProvider')]
+  public function testJsValue(string $expected, string $value, string $parameters = ''): void
+  {
+    $environment = self::makeEnvironment('{{- ' . $value . ' | js_value(' . $parameters . ') -}}', [
+      new ValuesExtension(),
+    ]);
+    self::assertRenders(
+      $expected,
+      $environment,
+      context: ['value' => $value],
+      message: 'The value should be encoded as an inline JS value.',
+    );
+  }
+
+  public static function jsValueDataProvider(): iterable
+  {
+    yield 'String' => [
+      'expected' => '"hello world"',
+      'value' => "'hello world'",
+    ];
+
+    yield 'Array' => [
+      'expected' => '["a","b","c"]',
+      'value' => "['a', 'b', 'c']",
+    ];
+
+    yield 'Object' => [
+      'expected' => '{"a":1,"b":2,"c":3}',
+      'value' => '{ a: 1, b: 2, c: 3 }',
+    ];
+
+    yield 'Pretty printed' => [
+      'expected' => <<<'TXT'
+      {
+          "a": 1,
+          "b": 2,
+          "c": 3
+      }
+      TXT
+      ,
+      'value' => '{ a: 1, b: 2, c: 3 }',
+      'parameters' => 'flags: constant("\JSON_PRETTY_PRINT")',
+    ];
+  }
+
   #[DataProvider('instanceOfDataProvider')]
   public function testInstanceOf(bool $expected, mixed $value, string $className): void
   {
